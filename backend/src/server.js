35b9f2853d119234860b77325c9b81dd01330cdf
Pe <nguyenbatho2k5@gmail.com>
2026-03-11 20:28:29 +0700

    init

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
